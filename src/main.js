47beab655d134eae4fa38b158fe372e35ef95b5c
huangbixia <15220007752@163.com>
2019-08-20 07:42:19 +0800

    <ADD>修改配置文件和安装包

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import 'babel-polyfill'// 转义ESAPI
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import './common/stylus/index.styl'
+import fastclick from 'fastclick'// 解决移动端点击延迟300毫秒
+
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
