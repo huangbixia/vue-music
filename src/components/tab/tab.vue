0448b4b9eee6400e069babf69306d82fb7f23324
huangbixia <15220007752@163.com>
2019-09-03 07:53:16 +0800

    <ADD>新建tab页

---
+++ src/components/tab/tab.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="tab">
+        <!-- tag=div 渲染成一个div标签，默认是a标签 -->
+        <router-link tag="div" class="tab-item" to="/recommend">
+            <span class="tab-link">推荐</span>
+        </router-link>
+        <router-link tag="div" class="tab-item" to="/singer">
+            <span class="tab-link">歌手</span>
+        </router-link>
+        <router-link tag="div" class="tab-item" to="/rank">
+            <span class="tab-link">排行</span>
+        </router-link>
+        <router-link tag="div" class="tab-item" to="/search">
+            <span class="tab-link">搜索</span>
+        </router-link>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    
+</style>
