41b738747d14d988a411ddd5a1cdedf2ee182cb0
huangbixia <15220007752@163.com>
2019-09-05 07:41:05 +0800

    <ADD>增加轮播图

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,13 +1,67 @@
 <template>
-    <div>推荐页面</div>
+    <div class=recommend>
+        <div class="recommend-content">
+            <div class="slider-wrapper">
+                <slider></slider>
+            </div>
+            <div class="recommend-list">
+                <h1 class="list-title">热门歌单推荐</h1>
+                <ul></ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script type="text/ecmascript-6">
+import {getRecommend} from 'api/recommend'
+import {ERR_OK} from 'api/config'
+import Slider from 'base/slider/slider'
+
 export default {
-    
+    components: {
+        Slider
+    },
+    created() {
+        this._getRecommend()
+    },
+    methods: {
+        _getRecommend() {
+            getRecommend().then((res) => {
+                if (res.code === ERR_OK) {
+
+                }
+            })
+        }
+    }    
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-    
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        height: 0
+        padding-top: 40%
+        overflow: hidden
+        .slider-content
+          position: absolute
+          top: 0
+          left: 0
+          width: 100%
+          height: 100%
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
 </style>
